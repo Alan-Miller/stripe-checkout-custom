844ac9f6729dbf8d1ab0c2bb4e031af1759759b5
Alan-Miller <alan.ashman@gmail.com>
2017-10-27 00:11:43 -0600

    refactored working code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
+import logo from'./imgs/logo.svg';
+import pumpkins from './imgs/pumpkins.jpg';
 import './App.css';
 const stripePublicKey = process.env.REACT_APP_STRIPE_PUBLIC_KEY;
 
 class App extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = { token: null }
+    
+    this.stripeHandler = window.StripeCheckout.configure({
+      key: stripePublicKey,
+      token: this.onToken,
+      amount: 1111,
+      currency: 'usd',
+      locale: 'auto',
+      zipCode: true,
+      name: 'Pumpkins',
+      description: 'Buy these pumpkins please.',
+      image: pumpkins,
+    });
+  }
+
+  componentWillUnmount() {
+    this.stripeHandler.close();
+  }
+
   onToken = token => {
-    console.log('token', token);
     token.card = void 0;
+    this.setState({token});
     const amount = 999;
     axios.post('/api/payment', {token, amount})
     .then(response => { console.log('payment response', response) });
+  } 
+
+  onClickPay(e) {
+    e.preventDefault();
+    this.stripeHandler.open({});
   }
 
   render() {
+    let buttonText = this.state.token ? 'Thank you!' : 'Pay $11.11';
+
     return (
       <div className="App">
         <header className="header">
           <img src={logo} className="logo" alt="logo" />
-          <h1 className="welcome">Welcome to Stripe Checkout</h1>
+          <h1 className="welcome">Stripe Checkout (custom)</h1>
         </header>
 
-        <StripeCheckout
-          token={this.onToken}
-          stripeKey={stripePublicKey}
-          amount={999} />
+        <div 
+          className={this.state.token ? "stripeButton disabled" : "stripeButton"} 
+          onClick={this.state.token ? null : this.onClickPay.bind(this)}>
+          {buttonText}
+        </div>
       </div>
     );
   }
